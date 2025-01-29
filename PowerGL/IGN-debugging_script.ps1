2ac3352cab37c200f44d640872696ac869a02791
Micah Shonyo <micah.shonyo@outlook.com>
2025-01-29 17:08:54 -0500

    Actually removed the scripts

--- PowerGL/IGN-debugging_script.ps1
+++
@@ -1,8 +0,0 @@
-import-module C:\Github\HoseRenderer\PowerGL\PowerGL.psm1
-$bruh = @()
-$shape = New-PowerGLShape -shapename "Cube" -shapenum 0 -PosX 1.0 -posy 2.0 -posz 3.0 -RotX 60.0 -RotY 45.0 -RotZ 30.0
-$bruh += $shape
-initialize-PowerGL -phase 1
-initialize-PowerGL -phase 2 -shapesarray $bruh
-Start-PowerGL
-initialize-PowerGL -phase 3
